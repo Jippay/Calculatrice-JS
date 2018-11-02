bc25502b6772dcc61e9feb053e836cf1d0a8c7ea
dcl-jipay <dcl-jeanphig@18pixel.fr>
2018-11-02 21:04:23 +0100

    calculatrice fonctionnelle + verification chiffre

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+//variable pour la verification
+var chiffre1 = document.getElementById("champChiffre1");
+var chiffre2 = document.getElementById("champChiffre2");
 
+//verification si la saisie numéro 1 est un chiffre
+function verifNbre1() {
+    if (isNaN(chiffre1.value) == true){
+        chiffre1.value = "";
+        chiffre1.style.border ="2px solid red";//bordure rouge en cas de lettre
+    }else{
+        chiffre1.style.border ="2px solid blue";//bordure bleu si chiffre
+    };
+}
+//verification si la saisie numéro 2 est un chiffre
+function verifNbre2() {
+    if (isNaN(chiffre2.value) == true){
+        chiffre2.value = "";
+        chiffre2.style.border ="2px solid red";//bordure rouge en cas de lettre
+    }else{
+        chiffre2.style.border ="2px solid blue";//bordure bleu si chiffre
+    };
+}
+
+//fonction calculatrice
 function calc(){
-    var chiffre1 = parseFloat(document.forms["calculatrice"].elements["champChiffre1"].value);
+    var chiffre1 = parseFloat(document.forms["calculatrice"].elements["champChiffre1"].value);//recupération des chiffres saisie
     var chiffre2 = parseFloat(document.forms["calculatrice"].elements["champChiffre2"].value);
     var resultat
     if(document.forms["calculatrice"].elements["list"].value == "additionner") {
@@ -16,7 +39,6 @@ function calc(){
         var resultat = chiffre1 / chiffre2;
     }
     
-    document.forms["calculatrice"].elements["result"].value = resultat;
+    document.forms["calculatrice"].elements["result"].value = resultat;//affichage du resultat dans la zone resultat
 
 };
-
